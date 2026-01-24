04e771cb55b5c973ea8e07d5799be00bdbfb4d2f
zhao vistart <vistart@rho.social>
2026-01-24 17:42:53 +0800

    feat: remove old CTE query tests and add new CTE query ActiveQuery tests

--- tests/rhosocial/activerecord_test/feature/query/test_cte_queries.py
+++
@@ -1,8 +0,0 @@
-# tests/rhosocial/activerecord_test/feature/query/test_cte_queries.py
-"""
-Bridge file for CTE query tests from the testsuite.
-
-This file imports the generic tests from the testsuite package and makes them
-discoverable by pytest in this project's test run.
-"""
-from rhosocial.activerecord.testsuite.feature.query.test_cte_queries import *
