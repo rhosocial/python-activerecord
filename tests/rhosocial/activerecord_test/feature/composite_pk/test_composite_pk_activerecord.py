0cad0633018aed3c83b7bce3ecbd4a9057086df6
zhao vistart <i@vistart.me>
2026-06-16 10:47:31 +0800

    fix: make composite_pk bridge files gracefully skip when testsuite is missing

--- tests/rhosocial/activerecord_test/feature/composite_pk/test_composite_pk_activerecord.py
+++ tests/rhosocial/activerecord_test/feature/composite_pk/test_composite_pk_activerecord.py
@@ -1,2 +1,6 @@
 # Bridge file for composite_pk ActiveRecord CRUD tests
-from rhosocial.activerecord.testsuite.feature.composite_pk.test_activerecord import *  # noqa: F403
+import pytest
+try:
+    from rhosocial.activerecord.testsuite.feature.composite_pk.test_activerecord import *  # noqa: F403
+except ImportError:
+    pytest.skip("composite_pk testsuite not available", allow_module_level=True)
--- tests/rhosocial/activerecord_test/feature/composite_pk/test_composite_pk_cte_query.py
+++ tests/rhosocial/activerecord_test/feature/composite_pk/test_composite_pk_cte_query.py
@@ -1,2 +1,6 @@
 # Bridge file for composite_pk CTE query tests
-from rhosocial.activerecord.testsuite.feature.composite_pk.test_cte_query import *  # noqa: F403
+import pytest
+try:
+    from rhosocial.activerecord.testsuite.feature.composite_pk.test_cte_query import *  # noqa: F403
+except ImportError:
+    pytest.skip("composite_pk testsuite not available", allow_module_level=True)
